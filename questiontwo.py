d84ac0fe934db779cf218e9740c92e1daddf58c6
sam kariuki <samuel.kariuki1@student.moringaschool.com>
2023-08-27 20:00:51 +0300

    using set instead to select unique numbers

--- questiontwo.py
+++ questiontwo.py
@@ -1,8 +1,14 @@
 def remove_duplicates(sequence):
-    result = []  # Initialize an empty list to store unique elements
+    seen = set()  # Initialize an empty set to store unique elements
+    result = []   # Initialize an empty list to store the result
+
     # Iterate through the elements in the input sequence
-    [result.append(x) for x in sequence if x not in result]
-    return result  # Return the list with duplicates removed
+    for item in sequence:
+        if item not in seen:  # Check if the item is not already seen
+            seen.add(item)    # Add the item to the set to mark it as seen
+            result.append(item)  # Append it to the result list
+
+    return result
 
 # Test case
 input_sequence = [2, 3, 2, 4, 5, 3, 6, 7, 5]
